d8bb08dd417f519dc3b281c8ea7fcbc9c3f48541
huyue <huyuemiss@gmail.com>
2016-01-03 23:28:32 +0800

    Add

---
+++ Config/System.conf.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Description: Main configuration file.
+ * Created by : Hu Yue(yuehu@cloudinnov.com)
+ * Datetime   : 2016/01/03 23:15 
+ */
+
+define('VERSION', 'V0.0.1');
---
+++ Config/User.conf.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Description: User custom configuration file.
+ * Created by : Hu Yue(yuehu@cloudinnov.com)
+ * Datetime   : 2016/01/03 23:15
+ */
